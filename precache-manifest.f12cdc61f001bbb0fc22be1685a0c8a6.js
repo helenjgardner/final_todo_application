634f8891fe415c9254586b53079d2cfdd20d772c
helenjgardner <helen_gardner@hotmail.com>
2019-11-09 13:51:47 +0000

    Updates

---
+++ precache-manifest.f12cdc61f001bbb0fc22be1685a0c8a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d3fbfdadfdb3e1bb401f7afb780718",
+    "url": "/final_todo_application/index.html"
+  },
+  {
+    "revision": "d562601829cd07e06c87",
+    "url": "/final_todo_application/static/css/main.9a2b1540.chunk.css"
+  },
+  {
+    "revision": "3ec16a1a39bcaec8a8a1",
+    "url": "/final_todo_application/static/js/2.069cc6b7.chunk.js"
+  },
+  {
+    "revision": "d562601829cd07e06c87",
+    "url": "/final_todo_application/static/js/main.ba51cf68.chunk.js"
+  },
+  {
+    "revision": "11f9cf08a80fca5f4a2c",
+    "url": "/final_todo_application/static/js/runtime-main.86c0646d.js"
+  }
+]);
